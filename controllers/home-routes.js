f22073e0d78bc535991052845e6488ac11c3c951
Vienna Borowska <vienna.borowska@hotmail.com>
2022-02-21 17:59:30 +0000

    added single recipe route, started building single recipe card

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,9 +3,35 @@ const { User, Recipe, Comments } = require("../models");
 module.exports = router;
 
 router.get("/", async (req, res) => {
-    try {
-      res.render("dashboard")
-    } catch (err) {
-      res.sendStatus(500).send(err);
-    }
+  try {
+    res.render("dashboard");
+  } catch (err) {
+    res.sendStatus(500).send(err);
+  }
+});
+
+// Get Single Recipe
+router.get("/:id", async (req, res) => {
+  try {
+    const recipe = (
+      await Recipe.findOne({
+        where: { id: req.params.id },
+        attributes: [
+          "id",
+          "recipe_title",
+          "recipe_cooking_time",
+          "recipe_serves",
+          "recipe_summary",
+          "recipe_ingredients",
+          "recipe_method",
+        ],
+      })
+    ).get({ plain: true });
+    res.render("single-recipe", {
+      ...recipe,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
