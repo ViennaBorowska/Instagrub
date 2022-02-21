049c0074eb8189eddb07a7371aa26cb9a059ee24
mattglwilliams <mattwilliamsdev@gmail.com>
2022-02-21 17:52:33 +0000

    Styled the add recipe page and added event listeners

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,9 +3,17 @@ const { User, Recipe, Comments } = require("../models");
 module.exports = router;
 
 router.get("/", async (req, res) => {
-    try {
-      res.render("dashboard")
-    } catch (err) {
-      res.sendStatus(500).send(err);
-    }
+  try {
+    res.render("dashboard");
+  } catch (err) {
+    res.sendStatus(500).send(err);
+  }
+});
+
+router.get("/add-recipe", async (req, res) => {
+  try {
+    res.render("add-recipe");
+  } catch (err) {
+    res.sendStatus(500).send(err);
+  }
 });
