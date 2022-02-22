b3635bfa9644951a9b7e2637262f47c256793552
Sampreeti Das <sampreetighosh@gmail.com>
2022-02-22 18:21:31 +0000

    Updated User profile image upload

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,9 +3,31 @@ const { User, Recipe, Comments } = require("../models");
 module.exports = router;
 
 router.get("/", async (req, res) => {
-    try {
-      res.render("dashboard")
-    } catch (err) {
-      res.sendStatus(500).send(err);
-    }
+  try {
+    res.render("dashboard");
+  } catch (err) {
+    res.sendStatus(500).send(err);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const userFromDb = await User.findByPk(req.params.id, {
+      include: [
+        {
+          model: Recipe,
+          attributes: ["id", "recipe_title"],
+        },
+      ],
+    });
+    //res.status(200).json(userFromDb);
+    const user = userFromDb.get({ plain: true });
+
+    return res.render("profile", {
+      ...user,
+    });
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500).send(err);
+  }
 });
