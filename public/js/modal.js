67ba1dd985fc5548dce2f5ffa9a119452c0fb737
Alistair Houghton <alistair.g.houghton@gmail.com>
2022-03-01 21:47:54 +0000

    added ids

--- public/js/modal.js
+++ public/js/modal.js
@@ -17,3 +17,36 @@ window.onclick = (e) => {
     }
   });
 };  
+
+// const commentBtn = document.getElementById("single-recipe-comment-button");
+
+// commentBtn.addEventListener("click", async (event) => {
+//   event.preventDefault();
+
+//   console.log("add comment button pressed");
+
+//   const commentContent = document
+//     .getElementById("single-recipe-comment-input")
+//     .value.trim();
+//   // const recipe_id = window.location.toString().split("/").pop();
+//   const recipe_id = document.getElementsByClassName("recipe-card-id-span").value.trim();
+
+  
+//     console.log("recipe id", recipe_id);
+ 
+
+//   const newComment = await fetch("/api/comments", {
+//     method: "POST",
+//     body: JSON.stringify({
+//       comment_desc: commentContent,
+//       recipe_id: recipe_id,
+//     }),
+//     headers: { "Content-Type": "application/json" },
+//   });
+//   if (newComment.ok) {
+//     // alert("comment successful");
+//     document.location.replace(`/recipe/${recipe_id}`);
+//   } else {
+//     alert("comment failed");
+//   }
+// });
