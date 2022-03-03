df788498cf8b77c5eb574deb5222cb1b54a605d7
Vienna Borowska <vienna.borowska@hotmail.com>
2022-03-03 18:20:15 +0000

    comment-out script not working - check id to render update/delete buttons on recipe page

--- public/js/single-recipe.js
+++ public/js/single-recipe.js
@@ -1,16 +1,16 @@
-const { User, Recipe } = require("../../models");
+// const { User, Recipe } = require("../../models");
 
-// Handlebars.registerHelper("ifEquals", function (arg1, arg2, options) {
-//   return arg1 == arg2 ? options.fn(this) : options.inverse(this);
+// // Handlebars.registerHelper("ifEquals", function (arg1, arg2, options) {
+// //   return arg1 == arg2 ? options.fn(this) : options.inverse(this);
+// // });
+
+// var data = {
+//   userId: User.id,
+// };
+// console.log("----------------------", User.id);
+
+// $.extend(data, {
+//   isUserId: function () {
+//     return this.userId === Recipe.user_id;
+//   },
 // });
-
-var data = {
-  userId: User.id,
-};
-console.log("----------------------", User.id);
-
-$.extend(data, {
-  isUserId: function () {
-    return this.userId === Recipe.user_id;
-  },
-});
